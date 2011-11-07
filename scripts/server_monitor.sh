e34d9219e0bfac246e65176d64ab109eefc7180f
Stein Inge Morisbak <stein.inge.morisbak@BEKK.no>
2011-11-07 22:00:36 +0100

    Simpler monitor sed expression

--- scripts/server_monitor.sh
+++ scripts/server_monitor.sh
@@ -7,12 +7,10 @@
 # Author: Stein Inge Morisbak (stein.inge.morisbak@BEKK.no)
 #
 
-JAVA_HOME=$(/usr/libexec/java_home)
 APP_HOME=/server/bekkopen/webapp
 
 hostname=`hostname`
-PID=`ps -ea -o "pid ppid args" | grep -v grep | grep "${JAVA_HOME}/bin/java" \
-  | grep "classpath :${APP_HOME}/etc" | sed -e 's/^  *//' -e 's/ .*//' | head -1`
+PID=`ps -ea -o "pid ppid args" | grep -v grep | grep "classpath :${APP_HOME}/etc" | sed -e 's/^  *//' -e 's/ .*//' | head -1`
 if [ ! "$PID" ]; then
   echo "Sever $hostname is down." | mail -s "Server $hostname is down (eom)" "stein.inge.morisbak@BEKK.no"
 fi
